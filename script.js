dd33752163462a3dd8f0b966d8c7612983c24f5f
Arslan Mumtaz <arslanmumtaz0000@gmail.com>
2024-07-06 23:38:17 +0500

    done with the basics

---
+++ script.js
@@ -0,0 +1,13 @@
+//Pillars of DOM
+
+// >1 Selection of an Element
+
+var a = document.querySelector("h1");
+a.innerHTML = "ma to change ho gya"
+a.style.color = "black"
+a.style.backgroundColor = "khaki"
+
+
+// >2 Changing HTML
+// >3 Changing CSS
+// >4 Event Listener
